2e2cb6e5e0118079d09d4d25968ab083b263cfee
engapantio <bohdankozar@zohomail.eu>
2025-09-19 22:09:00 +0200

    add default tags and style imports

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,34 +1,17 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import css from "./App.module.css";
-import "modern-normalize";
-
+import css from './App.module.css';
+import 'modern-normalize';
+import SearchBar from '../SearchBar/SearchBar';
+import MovieGrid from '../MovieGrid/MovieGrid';
+import Loader from '../Loader/Loader';
+import ErrorMessage from '../ErrorMessage/ErrorMessage';
+console.log(css);
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <SearchBar />
+      <MovieGrid />
+      <Loader />
+      <ErrorMessage />
     </>
   );
 }
